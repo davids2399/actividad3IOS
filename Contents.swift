53a79b865d6336c88ee847716ad551db718ac5a4
llWolfz <davidsagozz@hotmail.com>
2020-02-01 22:01:01 -0600

    Actividad 3

---
+++ Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Declaracion explicita
+var cadena: String = String()
+// Inferida
+cadena = "David Alan Salazar"
+
+var number: Int
+number = 10
+
+var decimal: Float
+decimal = 15.05
+
+// Asociacion
+var myFullName = cadena + " Gonzalez"
+var edad = number + 10
+
+
+//Arreglo
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let diasSemana = [1: "Lunes", 2: "Martes", 3: "Miercoles", 4: "Jueves", 5: "Viernes", 6: "Sabado", 7: "Domingo"]
